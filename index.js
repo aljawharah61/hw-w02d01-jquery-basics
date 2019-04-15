07a0a2d649d22f8ff34963578150457f0caf6d34
Aljawharah <aljawharah61@gmail.com>
2019-04-15 09:09:16 +0300

    HW W02 D01

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-// turn on stop function 
+
+
+$('#stopButton').on('click',turnOnStopLight);
+$('#slowButton').on('click',turnOnSlowLight);
+$('#goButton').on('click',turnOnGoLight);
+
 function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
+
+
+$('#stopLight').css('background-color','Red');
+$('#slowLight').css('background-color','Black');
+$('#goLight').css('background-color','Black');
 }
 
 // turn on slow function
 function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+  $('#stopLight').css('background-color','Black');
+  $('#slowLight').css('background-color','yellow');
+  $('#goLight').css('background-color','Black');
 }
-
+  
 // turn on go function
 function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
-}
+  $('#stopLight').css('background-color','Black');
+  $('#slowLight').css('background-color','Black');
+  $('#goLight').css('background-color','Green');}
 
 // set stop light click event
 
 // set slow light click event
 
 // set go light click event
+
 
